afd4c928c8d34e9e0de896bb0b515cddc0342550
Firehawk <hooovercat@gmail.com>
2013-10-21 13:21:35 +0200

    filecreator

---
+++ code/apsi_lab1/src/ch/fhnw/apsi/lab1/FileCreator.java
@@ -0,0 +1,16 @@
+package ch.fhnw.apsi.lab1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class FileCreator {
+
+	HashMap<Integer,ArrayList<String>> map;
+	
+	private void fillMap() {
+		
+	}
+	
+	
+	
+}
